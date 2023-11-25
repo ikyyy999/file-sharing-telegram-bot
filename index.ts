33a42345c8916a1c317c879d61ab43fdc16b1ea9
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-25 11:12:17 +0800

    index.ts

--- index.ts
+++ index.ts
@@ -6,36 +6,22 @@ import sendMediaFunction from "./utils/sendMediaFunction";
 
 const bot = new Bot(botToken);
 
-bot.command("start", async (ctx: Context) => {
+bot.command("start", async (ctx) => {
   try {
-    const userId = ctx.from?.id; // Use optional chaining to handle 'undefined'
-    
-    if (!userId) {
-      // Handle the case where ctx.from is undefined
-      console.error("User ID not available");
-      return;
-    }
-
-    // Check if the user is a member of the channel
-    const isMember = await bot.api.getChatMember(channelUsername, userId);
-    if (isMember.status !== "member" && isMember.status !== "administrator") {
-      await ctx.reply("To use this bot, you must join our channel first!");
-      return;
-    }
-
     if (ctx.match && ctx.match.length === 8) {
       const file = await getFile(ctx.match);
       if (!file) {
-        await ctx.reply("Code not found! Please make sure your code is correct.");
+        await ctx.reply(
+          "Code not found! Please make sure your code is correct."
+        );
         return;
       }
       await ctx.reply("Getting your file, please wait a moment");
       await sendMediaFunction(ctx, file);
       return;
     }
-
     return ctx.reply(
-      "Welcome to the file-sharing Telegram bot! Just upload your file that you want to share."
+      "Welcome to file sharing telegram bot! Just upload your file that you want to share."
     );
   } catch (error) {
     console.error(error);
@@ -43,4 +29,36 @@ bot.command("start", async (ctx: Context) => {
   }
 });
 
-// ... (Other parts of your code remain unchanged)
+bot.on("message:text", async (ctx) => {
+  await ctx.reply(
+    "I don't understand your input :(. Please directly upload your file that you want to share :D"
+  );
+});
+
+bot.on("message:file", async (ctx) => {
+  try {
+    const file = await ctx.getFile();
+    const fileCode = await storeFile(file.file_id);
+    return ctx.reply(
+      `Your file has been stored with code: ${fileCode}. You can share the file using this link https://t.me/${botID}?start=${fileCode}`
+    );
+  } catch (error) {
+    console.error(error);
+    await ctx.reply("Something wrong! Please try again :(");
+  }
+});
+
+if (process.env.NODE_ENV === "production") {
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  bot.start();
+}
+
+console.log("The bot is running 🚀️🚀️🚀️");
