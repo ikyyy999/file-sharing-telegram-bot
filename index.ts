e24e938e2fbe9b9034faad39bc7fb351514672f7
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-25 12:00:38 +0800

    index.ts

--- index.ts
+++ index.ts
@@ -1,61 +1,62 @@
-// Impor modul dan fungsi yang diperlukan
-import { Bot, webhookCallback, Context } from "grammy"; // Impor Context dari grammY
+import { Bot, webhookCallback } from "grammy";
 import express from "express";
-import { getFile, storeFile } from "./services";
+import { getFile, storeFile, getFileByCode } from "./services"; // Tambahkan fungsi getFileByCode
 import { botID, botToken, adminIDs } from "./config";
 import sendMediaFunction from "./utils/sendMediaFunction";
 
-// Membuat instance Bot baru
 const bot = new Bot(botToken);
 
-// Menangani perintah start
-bot.command("start", async (ctx: Context) => { // Tambahkan anotasi tipe untuk 'ctx'
+bot.command("start", async (ctx) => {
   try {
-    // ... (Bagian tersisa dari perintah start tetap tidak berubah)
+    if (ctx.match && ctx.match.length === 8) {
+      const fileCode = ctx.match; // Ambil kode dari tautan yang diberikan oleh pengguna
+      const file = await getFileByCode(fileCode);
+      
+      if (!file) {
+        await ctx.reply("File not found! Please make sure the code is correct.");
+        return;
+      }
+
+      // Kirim file ke pengguna
+      await sendMediaFunction(ctx, file);
+      return;
+    }
+
+    return ctx.reply("Welcome to the file-sharing Telegram bot! Just upload your file that you want to share.");
   } catch (error) {
     console.error(error);
-    await ctx.reply("Ada kesalahan! Silakan coba lagi :(");
+    await ctx.reply("Something wrong! Please try again :(");
   }
 });
 
-// Menangani pesan teks
-bot.on("message:text", async (ctx: Context) => { // Tambahkan anotasi tipe untuk 'ctx'
-  await ctx.reply(
-    "Saya tidak mengerti input Anda :(. Silakan langsung unggah file yang ingin Anda bagikan :D"
-  );
+bot.on("message:text", async (ctx) => {
+  await ctx.reply("I don't understand your input :(. Please directly upload your file that you want to share :D");
 });
 
-// Menangani pesan file
-bot.on("message:file", async (ctx: Context) => { // Tambahkan anotasi tipe untuk 'ctx'
+bot.on("message:file", async (ctx) => {
   try {
     // Periksa apakah pengirim adalah admin
     const isAdmin = adminIDs && adminIDs.includes(ctx.from?.id?.toString() || "");
 
-    if (isAdmin) {
-      // Jika pengirim adalah admin, lanjutkan seperti biasa
-      const file = await ctx.getFile();
-      const fileCode = await storeFile(file.file_id);
-      return ctx.reply(
-        `File Anda telah disimpan dengan kode: ${fileCode}. Anda dapat berbagi file menggunakan tautan ini https://t.me/${botID}?start=${fileCode}`
-      );
-    } else {
-      // Jika pengirim bukan admin, izinkan pengguna mengakses tautan
-      const fileId = ctx.message?.document?.file_id;
-      if (fileId) {
-        return ctx.reply(
-          `Anda dapat mengakses file menggunakan tautan ini: https://t.me/${botID}?start=${fileId}`
-        );
-      } else {
-        return ctx.reply("Maaf, saya tidak dapat memproses file tersebut. Silakan coba lagi.");
-      }
+    if (!isAdmin) {
+      await ctx.reply("Only admins can send files.");
+      return;
     }
+
+    const file = await ctx.getFile();
+    const fileCode = await storeFile(file.file_id);
+
+    // Berikan link yang dapat diakses pengguna
+    const fileLink = `https://t.me/${botID}?start=${fileCode}`;
+    
+    return ctx.reply(`Your file has been stored with code: ${fileCode}. You can share the file using this link ${fileLink}`);
   } catch (error) {
     console.error(error);
-    await ctx.reply("Ada kesalahan! Silakan coba lagi :(");
+    await ctx.reply("Something wrong! Please try again :(");
   }
 });
 
-// Jalankan bot atau atur webhook di produksi
+// Handle webhook or start the bot in development
 if (process.env.NODE_ENV === "production") {
   const app = express();
   app.use(express.json());
