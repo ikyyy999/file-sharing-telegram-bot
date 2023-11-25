084a85b82476ed32efdc5920c13e909e19665dfe
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-25 12:30:43 +0800

    index.ts

--- index.ts
+++ index.ts
@@ -1,48 +1,32 @@
-import { Bot, webhookCallbackimport, ChatMemberStatus } from "grammy";
+import { Bot, webhookCallback } from "grammy";
 import express from "express";
 import { getFile, storeFile, getFileByCode } from "./services";
-import { botID, botToken, adminIDs, channelUsername } from "./config";
+import { botID, botToken, adminIDs, channelLink } from "./config"; // Sesuaikan dengan konfigurasi Anda
 import sendMediaFunction from "./utils/sendMediaFunction";
 
 const bot = new Bot(botToken);
-
-async function checkSubscription(userId: number): Promise<boolean> {
-  try {
-    const chatMember = await bot.api.getChatMember(channelUsername, userId);
-
-    // Periksa apakah status anggota adalah "member" atau "administrator"
-    const isSubscriber = (
-      chatMember.status === ChatMemberStatus.Member ||
-      chatMember.status === ChatMemberStatus.Administrator
-    );
-
-    return isSubscriber;
-  } catch (error) {
-    console.error("Error checking subscription:", error);
-    return false; // Mengembalikan false jika terjadi kesalahan
-  }
-}
 
 bot.command("start", async (ctx) => {
   try {
     if (ctx.match && ctx.match.length === 8) {
       const fileCode = ctx.match;
-      const fileId = await getFileByCode(fileCode);
+      const file = await getFileByCode(fileCode);
 
-      if (!fileId) {
+      if (!file) {
         await ctx.reply("File not found! Please make sure the code is correct.");
         return;
       }
 
-      const userId = ctx.from?.id || 0;
-      const isSubscribed = await checkSubscription(userId);
-
-      if (!isSubscribed) {
-        await ctx.reply("You need to subscribe to access this file.");
+      // Cek apakah pengguna sudah berlangganan ke saluran
+      const isSubscribed = await ctx.getChatMember(ctx.from!.id, channelLink);
+      
+      if (isSubscribed.status !== 'member' && isSubscribed.status !== 'administrator') {
+        // Jika tidak berlangganan, minta pengguna untuk berlangganan terlebih dahulu
+        await ctx.reply(`Anda harus berlangganan di ${channelLink} untuk mengakses file.`);
         return;
       }
 
-      const file = await getFile(fileId);
+      // Kirim file ke pengguna
       await sendMediaFunction(ctx, file);
       return;
     }
@@ -54,6 +38,34 @@ bot.command("start", async (ctx) => {
   }
 });
 
+bot.on("message:text", async (ctx) => {
+  await ctx.reply("I don't understand your input :(. Please directly upload your file that you want to share :D");
+});
+
+bot.on("message:file", async (ctx) => {
+  try {
+    // Periksa apakah pengirim adalah admin
+    const isAdmin = adminIDs && adminIDs.includes(ctx.from?.id?.toString() || "");
+
+    if (!isAdmin) {
+      await ctx.reply("Only admins can send files.");
+      return;
+    }
+
+    const file = await ctx.getFile();
+    const fileCode = await storeFile(file.file_id);
+
+    // Berikan link yang dapat diakses pengguna
+    const fileLink = `https://t.me/${botID}?start=${fileCode}`;
+    
+    return ctx.reply(`Your file has been stored with code: ${fileCode}. You can share the file using this link ${fileLink}`);
+  } catch (error) {
+    console.error(error);
+    await ctx.reply("Something wrong! Please try again :(");
+  }
+});
+
+// Handle webhook or start the bot in development
 if (process.env.NODE_ENV === "production") {
   const app = express();
   app.use(express.json());
