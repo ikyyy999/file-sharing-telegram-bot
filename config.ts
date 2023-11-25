2babd1350b824bc5678ed2461a17546f3d77d12f
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-25 10:43:31 +0800

    config.ts

--- config.ts
+++ config.ts
@@ -3,4 +3,5 @@ dotenv.config();
 
 export const botToken: any = process.env.BOT_TOKEN;
 export const cyclicDBName = process.env.CYCLIC_DB_NAME;
+export const channelUsername = process.env.FSUB_CHANNEL;
 export const botID = process.env.BOT_ID;
