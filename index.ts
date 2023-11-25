847b60b6b5fcd2c92649afda22081600ed69dc9c
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-25 11:53:49 +0800

    index.ts

--- index.ts
+++ index.ts
@@ -1,37 +1,57 @@
 // Impor modul dan fungsi yang diperlukan
-import { Bot, webhookCallback } from "grammy";
+import { Bot, webhookCallback, Context } from "grammy"; // Impor Context dari grammY
 import express from "express";
 import { getFile, storeFile } from "./services";
-import { botID, botToken, adminIDs } from "./config"; // Mengasumsikan adminIDs didefinisikan dalam konfigurasi Anda
-
+import { botID, botToken, adminIDs } from "./config";
 import sendMediaFunction from "./utils/sendMediaFunction";
 
-bot.on("message:file", async (ctx) => {
+// Membuat instance Bot baru
+const bot = new Bot(botToken);
+
+// Menangani perintah start
+bot.command("start", async (ctx: Context) => { // Tambahkan anotasi tipe untuk 'ctx'
+  try {
+    // ... (Bagian tersisa dari perintah start tetap tidak berubah)
+  } catch (error) {
+    console.error(error);
+    await ctx.reply("Ada kesalahan! Silakan coba lagi :(");
+  }
+});
+
+// Menangani pesan teks
+bot.on("message:text", async (ctx: Context) => { // Tambahkan anotasi tipe untuk 'ctx'
+  await ctx.reply(
+    "Saya tidak mengerti input Anda :(. Silakan langsung unggah file yang ingin Anda bagikan :D"
+  );
+});
+
+// Menangani pesan file
+bot.on("message:file", async (ctx: Context) => { // Tambahkan anotasi tipe untuk 'ctx'
   try {
     // Periksa apakah pengirim adalah admin
     const isAdmin = adminIDs && adminIDs.includes(ctx.from?.id?.toString() || "");
 
     if (isAdmin) {
-      // Jika pengirim adalah admin, proses seperti biasa
+      // Jika pengirim adalah admin, lanjutkan seperti biasa
       const file = await ctx.getFile();
       const fileCode = await storeFile(file.file_id);
       return ctx.reply(
-        `Your file has been stored with code: ${fileCode}. You can share the file using this link https://t.me/${botID}?start=${fileCode}`
+        `File Anda telah disimpan dengan kode: ${fileCode}. Anda dapat berbagi file menggunakan tautan ini https://t.me/${botID}?start=${fileCode}`
       );
     } else {
-      // Jika pengirim bukan admin, izinkan pengguna mengakses tautan yang dikirimkan
+      // Jika pengirim bukan admin, izinkan pengguna mengakses tautan
       const fileId = ctx.message?.document?.file_id;
       if (fileId) {
         return ctx.reply(
-          `You can access the file using this link: https://t.me/${botID}?start=${fileId}`
+          `Anda dapat mengakses file menggunakan tautan ini: https://t.me/${botID}?start=${fileId}`
         );
       } else {
-        return ctx.reply("Sorry, I couldn't process the file. Please try again.");
+        return ctx.reply("Maaf, saya tidak dapat memproses file tersebut. Silakan coba lagi.");
       }
     }
   } catch (error) {
     console.error(error);
-    await ctx.reply("Something wrong! Please try again :(");
+    await ctx.reply("Ada kesalahan! Silakan coba lagi :(");
   }
 });
 
