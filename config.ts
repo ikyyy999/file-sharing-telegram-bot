a8e5d967c7af658c89c036ca7e68855e23ecba0a
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-29 18:20:31 +0800

    config.ts

--- config.ts
+++ config.ts
@@ -6,3 +6,4 @@ export const cyclicDBName = process.env.CYCLIC_DB_NAME;
 export const channelUsername = process.env.FSUB_CHANNEL;
 export const adminIDs = process.env.ADMIN_ID;
 export const botID = process.env.BOT_ID;
+export const telegramApiId = process.env.API_ID;
