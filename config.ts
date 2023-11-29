66767b16279984c0128bda75e2ac70c87b20cd8d
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-29 19:02:23 +0800

    config.ts

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const botToken: any = process.env.BOT_TOKEN;
 export const cyclicDBName = process.env.CYCLIC_DB_NAME;
-export const channelUsername = process.env.FSUB_CHANNEL;
+export const FORCE_SUB_CHANNEL = process.env.FSUB_CHANNEL;
 export const adminIDs = process.env.ADMIN_ID;
 export const botID = process.env.BOT_ID;
 export const telegramApiId = process.env.API_ID;
