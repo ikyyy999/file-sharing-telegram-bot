7aaf3934b581f89111cfad23e0964a9d7985ae4e
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-29 19:05:37 +0800

    index.ts

--- index.ts
+++ index.ts
@@ -1,53 +1,55 @@
 import { Bot, webhookCallback } from "grammy";
 import express from "express";
-import { getFile, storeFile, getFileByCode } from "./services";
-import { botID, botToken, adminIDs, FORCE_SUB_CHANNEL} from "./config";
+import { getFile, storeFile, getFileByCode } from "./services"; // Tambahkan fungsi getFileByCode
+import { botID, botToken, adminIDs } from "./config";
 import sendMediaFunction from "./utils/sendMediaFunction";
 
-class MyTelegramBot extends Bot {
-  constructor() {
-    super(botToken);
-  }
+const bot = new Bot(botToken);
 
-  async start() {
-    try {
-      await super.start();
-      const botInfo = await this.getMe();
-      this.username = botInfo.username;
-      this.namebot = botInfo.first_name;
-      console.log(`TG_BOT_TOKEN detected!\n┌ First Name: ${this.namebot}\n└ Username: @${this.username}\n——`);
-    } catch (error) {
-      console.error(error);
-      console.log("Bot stopped. Join https://t.me/SharingUserbot for assistance");
-      process.exit();
+bot.command("start", async (ctx) => {
+  try {
+    if (ctx.match && ctx.match.length === 8) {
+      const fileCode = ctx.match; // Ambil kode dari tautan yang diberikan oleh pengguna
+      const file = await getFileByCode(fileCode);
+      
+      if (!file) {
+        await ctx.reply("File not found! Please make sure the code is correct.");
+        return;
+      }
+
+      // Kirim file ke pengguna
+      await sendMediaFunction(ctx, file);
+      return;
     }
 
-    // Rest of the start method..
-
-    console.log("Bot is running 🚀️🚀️🚀️");
-  }
-
-  // Add other methods and functionalities here...
-}
-
-const myTelegramBot = new MyTelegramBot();
-
-myTelegramBot.command("start", async (ctx) => {
-  try {
-    // Your existing command logic...
+    return ctx.reply("Welcome to the file-sharing Telegram bot! Just upload your file that you want to share.");
   } catch (error) {
     console.error(error);
     await ctx.reply("Something wrong! Please try again :(");
   }
 });
 
-myTelegramBot.on("message:text", async (ctx) => {
+bot.on("message:text", async (ctx) => {
   await ctx.reply("I don't understand your input :(. Please directly upload your file that you want to share :D");
 });
 
-myTelegramBot.on("message:file", async (ctx) => {
+bot.on("message:file", async (ctx) => {
   try {
-    // Your existing file message logic...
+    // Periksa apakah pengirim adalah admin
+    const isAdmin = adminIDs && adminIDs.includes(ctx.from?.id?.toString() || "");
+
+    if (!isAdmin) {
+      await ctx.reply("Only admins can send files.");
+      return;
+    }
+
+    const file = await ctx.getFile();
+    const fileCode = await storeFile(file.file_id);
+
+    // Berikan link yang dapat diakses pengguna
+    const fileLink = `https://t.me/${botID}?start=${fileCode}`;
+    
+    return ctx.reply(`Your file has been stored with code: ${fileCode}. You can share the file using this link ${fileLink}`);
   } catch (error) {
     console.error(error);
     await ctx.reply("Something wrong! Please try again :(");
@@ -58,12 +60,14 @@ myTelegramBot.on("message:file", async (ctx) => {
 if (process.env.NODE_ENV === "production") {
   const app = express();
   app.use(express.json());
-  app.use(webhookCallback(myTelegramBot, "express"));
+  app.use(webhookCallback(bot, "express"));
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Bot listening on port ${PORT}`);
   });
 } else {
-  myTelegramBot.start();
+  bot.start();
 }
+
+console.log("The bot is running 🚀️🚀️🚀️");
