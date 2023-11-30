bd66fa782ea5c1c5d16ab1405ee844539dbcf745
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-11-30 09:42:31 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,59 +1,39 @@
 import { Bot, webhookCallback } from "grammy";
 import express from "express";
 import { getFile, storeFile, getFileByCode } from "./services"; // Tambahkan fungsi getFileByCode
-import { botID, botToken, adminIDs } from "./config";
+import { botID, botToken, adminIDs, } from "./config";
 import sendMediaFunction from "./utils/sendMediaFunction";
 
 const bot = new Bot(botToken);
 
 bot.command("start", async (ctx) => {
-  try {
-    if (ctx.match && ctx.match.length === 8) {
-      const fileCode = ctx.match; // Ambil kode dari tautan yang diberikan oleh penggunan
-      const file = await getFileByCode(fileCode);
-      
-      if (!file) {
-        await ctx.reply("File not found! Please make sure the code is correct.");
-        return;
-      }
+ try {
+  if (ctx.match && ctx.match.length === 8) {
+   const fileCode = ctx.match; // Ambil kode dari tautan yang diberikan oleh penggunan
+   const file = await getFileByCode(fileCode);
+    
+   if (!file) {
+    await ctx.reply("File not found! Please make sure the code is correct.");
+    return;
+   }
 
-      // Kirim file ke pengguna
-      await sendMediaFunction(ctx, file);
-      return;
-    }
+  // Periksa apakah pengguna telah berlangganan channel atau group
+  if (!await ctx.chat.isMember(channelID)) { // Ganti channelID dengan ID channel atau group yang ingin Anda force subscribe
+    await ctx.reply("Anda harus berlangganan channel atau group ini terlebih dahulu.");
+    await ctx.reply("Ketik /join untuk bergabung dengan channel atau group.");
+    return;
+   }
 
-    return ctx.reply("Welcome to the file-sharing Telegram bot! Just upload your file that you want to share.");
-  } catch (error) {
-    console.error(error);
-    await ctx.reply("Something wrong! Please try again :(");
-  }
-});
+  // Kirim file ke pengguna
+   await sendMediaFunction(ctx, file);
+   return;
+  }
 
-bot.on("message:text", async (ctx) => {
-  await ctx.reply("I don't understand your input :(. Please directly upload your file that you want to share :D");
-});
-
-bot.on("message:file", async (ctx) => {
-  try {
-    // Periksa apakah pengirim adalah admin
-    const isAdmin = adminIDs && adminIDs.includes(ctx.from?.id?.toString() || "");
-
-    if (!isAdmin) {
-      await ctx.reply("Only admins can send files.");
-      return;
-    }
-
-    const file = await ctx.getFile();
-    const fileCode = await storeFile(file.file_id);
-
-    // Berikan link yang dapat diakses pengguna
-    const fileLink = `https://t.me/${botID}?start=${fileCode}`;
-    
-    return ctx.reply(`Your file has been stored with code: ${fileCode}. You can share the file using this link ${fileLink}`);
-  } catch (error) {
-    console.error(error);
-    await ctx.reply("Something wrong! Please try again :(");
-  }
+  return ctx.reply("Welcome to the file-sharing Telegram bot! Just upload your file that you want to share.");
+ } catch (error) {
+  console.error(error);
+  await ctx.reply("Something wrong! Please try again :(");
+ }
 });
 
 // Handle webhook or start the bot in development
