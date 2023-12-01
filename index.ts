ae3edea06397ce272ca63dd78a5a050d62d5c71d
ikyyy999 <150502503+ikyyy999@users.noreply.github.com>
2023-12-01 17:25:49 +0800

    index.ts

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const bot = new Bot(botToken);
 bot.command("start", async (ctx) => {
   try {
     if (ctx.match && ctx.match.length === 8) {
-      const fileCode = ctx.match; // Ambil kode dari tautan yang diberikan oleh penggunan
+      const fileCode = ctx.match; // Ambil kode dari tautan yang diberikan oleh pengguna
       const file = await getFileByCode(fileCode);
       
       if (!file) {
